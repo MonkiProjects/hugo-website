349d10463feb310aa5c0d57c2d12def5aef4be51
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-01-05 01:18:12 +0100

    Fix npm install in Clever Cloud pre run hook

--- hugo.sh
+++ hugo.sh
@@ -1,4 +1,6 @@
- npm install
+cd themes/vex-hugo
+npm install
+cd ../..
  wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
