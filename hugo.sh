ea1d20be4c21e46943ee77992d4ead04b4eadde9
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-01-05 01:08:21 +0100

    Add npm install in Clever Cloud pre run hook

--- hugo.sh
+++ hugo.sh
@@ -1,3 +1,4 @@
+ npm install
  wget https://github.com/gohugoio/hugo/releases/download/v$HUGO_VERSION/hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 tar xvf hugo_extended_"$HUGO_VERSION"_Linux-64bit.tar.gz
 chmod +x ./hugo
